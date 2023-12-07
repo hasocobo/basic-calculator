b976785e11dfbe878098333d7b6083785d9a055d
Hasan Çoban <hasancoban13579@gmail.com>
2023-12-08 01:28:51 +0300

    Make pressed value display on the calculator screen

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 let firstNumber = 0,
     secondNumber = 0;
 
-let operator = "";
+let operator = "",
+    inputText = "";
 
+let inputQueue = [],
+    numberQueue = [],
+    signQueue = [];
+
+let buttons = document.querySelectorAll("#calculator-buttons button"),
+    textArea = document.querySelector('#text-area'),
+    backSpaceButton = document.querySelector('#back-space-button');
+
+buttons.forEach(button => {
+    if (button.id != 'back-space-button' 
+    && button.id != 'result-button') {
+        button.addEventListener('click', () => {
+            displayOnTheScreen(button);
+        });
+    }
+
+    else if (button.id == 'back-space-button') {
+        button.addEventListener('click', backSpace);
+    }
+
+    else if (button.id == 'result-button') {
+        button.addEventListener('click', showResult);
+    }
+
+})
 
 function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
@@ -37,4 +63,24 @@ function operate(firstNum = firstNumber, operator, secondNum){
             break;
     }
     firstNumber = result;
+}
+
+function displayOnTheScreen(button) {
+    inputQueue.push(button.textContent);
+    inputText += button.textContent;
+    updateTextArea(inputText);
+}
+
+function backSpace() {
+    inputText = inputText.substring(0, inputText.length - 1);
+    inputQueue.pop();
+    updateTextArea(inputText);
+}
+
+function showResult() {
+
+}
+
+function updateTextArea(inputText) {
+    textArea.textContent = inputText;
 }
