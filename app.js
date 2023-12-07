23d7f349dedc50ac42f3223dc45f8acf4eb0d7bc
Hasan Çoban <hasancoban13579@gmail.com>
2023-12-07 16:01:30 +0300

    Create a function for evaluation
    Created a function called operate to evaluate the result when 2 numbers and 1 operator is given.

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
+let firstNumber = 0,
+    secondNumber = 0;
+
+let operator = "";
+
+
 function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
 }
+
 function subtract(firstNumber, secondNumber) {
     return firstNumber - secondNumber;
 }
+
 function multiply(firstNumber, secondNumber) {
     return firstNumber * secondNumber;
 }
+
 function divide(firstNumber, secondNumber) {
     return firstNumber / secondNumber;
+}
+
+function operate(firstNum = firstNumber, operator, secondNum){
+    let result = 0;
+    switch(operator) {
+        case "+":
+            result = add(firstNum, secondNum);
+            break;
+        case "-":
+            result = subtract(firstNum, secondNum);
+            break;
+        case "x":
+            result = multiply(firstNum, secondNum);
+            break;
+        case "/":
+            result = subtract(firstNum, secondNum);
+            break;
+    }
+    firstNumber = result;
 }
